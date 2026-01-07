debc04108b66fab63125e0c5ad15474b18ad7b45
Frederick Vargas <frederickmvn@gmail.com>
2026-01-07 12:21:01 -0400

    Create integration test for Product component

--- ecommerce-project/src/pages/home/Product.jsx
+++ ecommerce-project/src/pages/home/Product.jsx
@@ -34,13 +34,18 @@ export function Product({ product, loadCart }) {
   return (
     <div className="product-container">
       <div className="product-image-container">
-        <img className="product-image" src={product.image} />
+        <img
+          data-testid="product-image"
+          className="product-image"
+          src={product.image}
+        />
       </div>
 
       <div className="product-name limit-text-to-2-lines">{product.name}</div>
 
       <div className="product-rating-container">
         <img
+          data-testid="product-rating-stars-image"
           className="product-rating-stars"
           src={`images/ratings/rating-${product.rating.stars * 10}.png`}
         />
